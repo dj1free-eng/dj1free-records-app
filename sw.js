b220c3d5887e26c38c0bb412ace9aaf5a360124d
dj1free-eng <dj1free@gmail.com>
2025-12-26 22:27:45 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+const CACHE = "dj1free-records-v1";
+const ASSETS = [
+  "./",
+  "index.html",
+  "artistas.html",
+  "artista.html",
+  "album.html",
+  "cancion.html",
+  "css/style.css",
+  "js/app.js",
+  "data/catalog.json",
+  "manifest.webmanifest"
+];
 
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request))
+  );
+});
